a63bce14b1008bc44cedd4b187bc7639393f5a5d
WillowArk <53582577+WillowArk@users.noreply.github.com>
2021-10-02 16:32:32 -0500

    Ye babie

--- app/src/main/java/com/example/myapplication/FoodTab.java
+++ app/src/main/java/com/example/myapplication/FoodTab.java
@@ -2,23 +2,37 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
+import android.app.Activity;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.TextView;
 
 public class FoodTab extends AppCompatActivity {
+
+    public void takePicture(View view)
+    {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_tab);
 
-        // Get the Intent that started this activity and extract the string
-        //Intent intent = getIntent();
-        //String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
-        // Capture the layout's TextView and set the string as its text
-        //TextView textView = findViewById(R.id.textView);
-        //textView.setText(message);
     }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void dispatchTakePictureIntent(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
+    }
+
 }
